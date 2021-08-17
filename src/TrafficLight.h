f0eb97acbdedbf302218a42df94e920b84f8f337
savy19 <savyasachi.g@gmail.com>
2021-08-16 23:56:04 -0400

    Define functions/variables in TrafficLight class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,6 +30,8 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
+enum TrafficLightPhase {red, green};
+
 class TrafficLight
 {
 public:
@@ -41,8 +43,14 @@ public:
     // typical behaviour methods
     void waitForGreen();
 
+    void simulate();
+
+    TrafficLightPhase getCurrentPhase();
+
 private:
     // typical behaviour methods
+
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
@@ -50,6 +58,8 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase;
+
 };
 
 #endif
